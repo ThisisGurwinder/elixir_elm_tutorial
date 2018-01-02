44076f2e2771ca937501a4e56d7771805d37ab13
Gurwinder <ThisisGurwinder@Gmail.com>
2018-01-02 13:39:16 +0530

    Update

---
+++ introduction/platform/lib/platform_web/controllers/player_session_controller.ex
@@ -0,0 +1,26 @@
+defmodule PlatformWeb.PlayerSessionController do
+	use PlatformWeb, :controller
+
+	def new(conn, _) do
+		render conn, "new.html"
+	end
+
+	def create(conn, %{"session" => %{"username" => username, "password" => password }}) do
+		case PlatformWeb.PlayerAuthController.sign_in_with_username_and_password(conn, user, pass, repo: Platform.Repo) do
+			{:ok, conn} ->
+				conn
+				|> put_flash(:info, "welcome back")
+				|> redirect(to: page_path(conn, :index))
+			{:error, _reason, conn} ->
+				conn
+				|> put_flash(:error, "Invalid username/password confirmation")
+				|> render("new.html")
+		end
+	end
+
+	def delete(conn, _) do
+		conn
+		|> PlatformWeb.PlayerAuthController.signout()
+		|> redirect(to: player_session_path(conn, :new))
+	end
+end 
