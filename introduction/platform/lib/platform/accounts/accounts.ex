19946f1700f93800df0b3ca55247feea1f470481
Gurwinder <ThisisGurwinder@Gmail.com>
2018-01-02 12:21:55 +0530

    Update

--- introduction/platform/lib/platform/accounts/accounts.ex
+++ introduction/platform/lib/platform/accounts/accounts.ex
@@ -11,7 +11,7 @@ defmodule Platform.Accounts do
 
   def create_player(attrs \\ %{}) do
     %Player{}
-    |> Player.changeset(attrs)
+    |> Player.registration_changeset(attrs)
     |> Repo.insert()
   end
 
