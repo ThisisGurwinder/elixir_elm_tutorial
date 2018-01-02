28548205c336a4257ad7f6b7340dd67059e76429
Gurwinder <ThisisGurwinder@Gmail.com>
2018-01-03 00:45:59 +0530

    Nv

--- introduction/platform/assets/js/app.js
+++ introduction/platform/assets/js/app.js
@@ -3,6 +3,6 @@ import Elm from './elm.js'
 
 const elmContainer = document.querySelector("#elm-container")
 
-if(elmcontainer)  {
+if(elmContainer)  {
 	const elmApplication = Elm.Main.embed(elmContainer);
 }
