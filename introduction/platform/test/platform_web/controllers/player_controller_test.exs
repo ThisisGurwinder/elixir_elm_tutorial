65cef4a6ac1ab72605ae72be833f6c388714985b
Gurwinder <ThisisGurwinder@Gmail.com>
2018-01-02 12:32:17 +0530

    Update

--- introduction/platform/test/platform_web/controllers/player_controller_test.exs
+++ introduction/platform/test/platform_web/controllers/player_controller_test.exs
@@ -15,7 +15,7 @@ defmodule PlatformWeb.PlayerControllerTest do
   describe "index" do
     test "lists all players", %{conn: conn} do
       conn = get conn, player_path(conn, :index)
-      asser t html_response(conn, 200) =~ "Listing Players"
+      assert html_response(conn, 200) =~ "Listing Players"
     end
   end
 
