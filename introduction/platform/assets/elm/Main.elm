82fa7050bbade471f960bfadff125e76c63d3a66
Gurwinder <ThisisGurwinder@Gmail.com>
2018-01-03 02:03:49 +0530

    Nv

--- introduction/platform/assets/elm/Main.elm
+++ introduction/platform/assets/elm/Main.elm
@@ -1,7 +1,29 @@
 module Main exposing (..)
 
-import Html exposing (Html, text)
+import Html exposing (..)
+import Html.Attributes exposing (..)
 
 main : Html msg
 main =
-  text "Hello from Elm!"
+  div []
+  	[h1 [] [ text "Games"]
+  	, gamesIndex model
+  	]
+
+model : List String
+model =
+	["Platform Game",
+	"Adventure Game"
+]
+
+gamesIndex : Html msg
+gamesIndex =
+	ul [ class "games-list" ] [ gamesList ]
+
+gamesList : Html msg
+gamesList = 
+	ul [ class "games-list" ] [ gamesListItem ]
+
+gamesListItem : Html msg
+gamesListItem =
+	li [] [ text "Platform Game" ]
